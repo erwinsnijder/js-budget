f8406a36ac93fc81aedd85e032678544fd53e276
erwin snijder <erwinsnijder_4@hotmail.com>
2019-07-04 13:07:20 +0200

    update

--- js/app.js
+++ js/app.js
@@ -17,10 +17,25 @@ class UI {
   }
   submitBudgetForm() {
     const value = this.budgetInput.value
-    if(value==='' || value <0){
+    if (value==='' || value <0){
       this.budgetFeedback.classList.add('showItem');
       this.budgetFeedback.innerHTML = `<p> Value cannot be empty or negative!</p>`;
+      
+      const self = this;
+      // console.log(this);
+
+      setTimeout(function(){
+        self.budgetFeedback.classList.remove('showItem');
+      }, 4000)
     }
+    else {
+      this.budgetAmount.textContent = value;
+      this.budgetInput.value = "";
+      this.showBalance();
+    }
+  }
+  showBalance() {
+  console.log(`dit is balans`);
   }
 }
 
