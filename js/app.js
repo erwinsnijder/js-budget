d615fea0bf72e92301b159455214de0cc0e625e7
erwin snijder <erwinsnijder_4@hotmail.com>
2019-07-04 13:28:43 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -35,7 +35,27 @@ class UI {
     }
   }
   showBalance() {
-  console.log(`dit is balans`);
+  const expense = this.totalExpense();
+  const total = parseInt(this.budgetAmount.textContent) - expense;
+  this.balanceAmount.textContent = total;
+  if(total<0) {
+    this.balance.classList.remove('showGreen', 'showBlack');
+    this.balance.classList.add('showRed');
+  }
+
+  else if(total>0) {
+    this.balance.classList.remove('showRed', 'showBlack');
+    this.balance.classList.add('showGreen');
+  }
+
+  if(total===0) {
+    this.balance.classList.remove('showRed', 'showGreen');
+    this.balance.classList.add('showBlack');
+  }
+  }
+  totalExpense(){
+    let total = 400;
+    return total;
   }
 }
 
